14de8ed546e3ba2ac9f56a9fb39d2f4df4bf4cb2
Sihle Socishe <sihlesocishe@gmail.com>
2020-01-15 22:31:25 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+$name = $_POST['name'];
+$visitor_email = $_POST['email'];
+$message = $_POST['message'];
+
+$email_from = 'socishe@cutag.co.za';
+$email_subject = "Form submission from a Portfolio";
+$email_body = "User Name: $name.\n".
+				"User Email: $visitor_email.\n".
+					"User Message: $message.\n";
+
+$to = "sihlesocishe@gmail.com";
+
+$headers = "From: $email_from \r\n";
+
+$headers .= "Reply To: $visitor_email \r\n";
+
+mail($to, $email_subject, $email_body, $headers);
+
+header("Location: Socishe.cutag.co.za");
+?>
